2685c5511594ca0e1ea47db3e373810f8ef9dc1f
AbdalrhmanAbdoAlhade <abdo.king22227@gmail.com>
2026-05-22 14:33:53 +0300

    Add: Core\Middleware interface

--- core/Middleware.php
+++ core/Middleware.php
@@ -2,7 +2,11 @@
 
 namespace Core;
 
-interface Middleware 
+interface Middleware
 {
+    /**
+     * Handle the incoming request.
+     * Return true to continue, or call Response:: and return false to abort.
+     */
     public function handle(): bool;
 }
